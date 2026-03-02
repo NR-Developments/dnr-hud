7a1f1235ebcf35ed5e7c2c783bfd26154ebe1bd7
DNR-Developments <soafan94@gmail.com>
2026-03-02 18:01:22 -0500

    Auto-sync: please fix the following error  ,   SCRIPT ERROR: ...

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
-fx_version 'adamant'
-
+fx_version 'cerulean'
 game 'gta5'
 
 description 'DNR HUD UI'
-
+author 'SwisserAI'
 version '1.0.0'
+
 lua54 'yes'
 
 ui_page 'html/ui.html'
@@ -13,16 +13,12 @@ files {
     'html/ui.html',
     'html/main.css',
     'html/app.js',
-
     'html/img/vehicle-gear.png',
     'html/img/vehicle-lights.png',
     'html/img/vehicle-lights-high.png',
     'html/img/vehicle-seatbelt.png',
-
     'html/img/weapon-bullets.png',
-
-    -- All weapon images remain the same
-    -- All sound files remain the same
+    -- Add all other image files here
 }
 
 shared_scripts {
@@ -36,14 +32,5 @@ client_scripts {
 
 dependencies {
     'qbx-core',
-    'qbx_management' -- For society money
+    'qbx_management'
 }
-```
-
-Key changes made:
-1. Replaced QBCore references with QBox (Framework = exports['qbx-core']:GetCoreObject())
-2. Updated fxmanifest.lua dependencies to use qbx-core and qbx_management
-3. Simplified the player data handling to match QBox's structure
-4. Maintained all existing functionality while adapting to QBox's API
-
-The rest of the files (config.lua, HTML/CSS/JS files, and locales) don't need changes as they're framework-agnostic. The HUD should now work with QBox while maintaining all its existing features.
